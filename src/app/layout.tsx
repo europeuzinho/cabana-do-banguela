9abbd1a46a5f9952ea46738a113cdcd8dc01c3a7
Europeu <Princethegames@gmail.com>
2025-07-22 00:32:07 +0000

    chat as tags link do favicon é <link rel="apple-touch-icon" sizes="180x1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html lang="pt" className="!scroll-smooth">
       <head>
         <link rel="icon" href="https://www.pngall.com/wp-content/uploads/8/How-To-Train-Your-Dragon-Toothless.png" type="image/png" sizes="any" />
+        <link rel="apple-touch-icon" href="https://www.pngall.com/wp-content/uploads/8/How-To-Train-Your-Dragon-Toothless.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
